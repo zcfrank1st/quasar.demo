0380ec13c8b91cfcb997f320cc2aec285796b56c
zcfrank1st <zcfrank1st@sina.com>
2017-02-17 15:10:42 +0800

    update demo

--- src/main/java/com/chaos/quasar/demo/Main.java
+++ src/main/java/com/chaos/quasar/demo/Main.java
@@ -1,7 +1,12 @@
 package com.chaos.quasar.demo;
 
 import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.FiberExecutorScheduler;
+import co.paralleluniverse.fibers.FiberForkJoinScheduler;
+import co.paralleluniverse.fibers.FiberTimedScheduler;
 import co.paralleluniverse.strands.SuspendableRunnable;
+
+import java.util.concurrent.Executors;
 
 /**
  * Created by zcfrank1st on 17/02/2017.
@@ -10,9 +15,14 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         new Fiber<Void>((SuspendableRunnable) () -> System.out.println("hello world")).start();
 
-        while (true) {
-            Thread.sleep(5000L);
-            System.out.println("============");
-        }
+        FiberExecutorScheduler scheduler = new FiberExecutorScheduler("", Executors.newSingleThreadExecutor());
+        scheduler.execute(() -> System.out.println(""));
+
+
+        FiberForkJoinScheduler forkJoinScheduler = new FiberForkJoinScheduler("", 12);
+
+        FiberTimedScheduler timedScheduler = new FiberTimedScheduler(scheduler);
+
+
     }
 }
